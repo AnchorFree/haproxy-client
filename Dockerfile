ae6d96cbfaa9d15a9ecfdaa897a1a055eae3f590
Vladimir Zorin <vladimir@deviant.guru>
2018-03-21 13:54:46 -0700

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM haproxy:1.7.9-alpine
+
+RUN apk --no-cache upgrade && apk add --no-cache ca-certificates openssl && mkdir /etc/haproxy && mkdir -p /opt/haproxy
+COPY entrypoint.sh /bin/
+COPY ./error-pages /opt/haproxy/error-pages
+
+ENTRYPOINT ["/bin/entrypoint.sh"]
+
+
---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# encodes hostname, e.g. hss-mi-prod-15-1 to a string h-hmip15-1
+export ERROR_PAGE_SELF_ID=$(hostname|awk -F "-" '{print substr($1,1,1)"-"substr($2,1,2) substr($3,1,1) $4"-"$5}')
+
+for e_page in /opt/haproxy/error-pages/*; do
+    sed -i "s/ERROR_PAGE_SELF_ID/${ERROR_PAGE_SELF_ID}/g" $e_page
+done
+
+cp -rf /opt/haproxy/error-pages /etc/haproxy/
+
+exec /usr/local/sbin/haproxy-systemd-wrapper -p /run/haproxy.pid -f /etc/haproxy/haproxy.cfg
