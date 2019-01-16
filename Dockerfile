0566aa1c2239d275d930ce6b5c835d6dc2ffb462
Ilya Trushchenko <i@whitehat.com.ua>
2019-01-15 17:47:48 -0800

    - update to 1.8.17 - add DNS_RESOLVER variable

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,7 @@
-FROM haproxy:1.8.12-alpine
+FROM haproxy:1.8.17-alpine
 
 RUN apk --no-cache upgrade && apk add --no-cache ca-certificates openssl && mkdir /etc/haproxy && mkdir -p /opt/haproxy
 COPY entrypoint.sh /bin/
 COPY ./error-pages /opt/haproxy/error-pages
 
 ENTRYPOINT ["/bin/entrypoint.sh"]
-
-
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@
 # encodes hostname, e.g. hss-mi-prod-15-1 to a string h-hmip15-1
 export ERROR_PAGE_SELF_ID=$(hostname|awk -F "-" '{print substr($1,1,1)"-"substr($2,1,2) substr($3,1,1) $4"-"$5}')
 
+export DNS_RESOLVER=$(grep nameserver /etc/resolv.conf | cut -f2 -d' ')
+
 for e_page in /opt/haproxy/error-pages/*; do
     sed -i "s/ERROR_PAGE_SELF_ID/${ERROR_PAGE_SELF_ID}/g" $e_page
 done
